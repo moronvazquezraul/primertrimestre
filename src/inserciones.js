9c50f262d333213c0e26828a46d286b8f3470d77
moronvazquezraul <91061415+moronvazquezraul@users.noreply.github.com>
2021-11-15 07:28:40 +0100

    Update inserciones.js

--- src/inserciones.js
+++ src/inserciones.js
@@ -4,36 +4,36 @@ db.productos.deleteMany()
 
 db.productos.insertOne(
     {   _id:01,
-        Nombre:"Xiaomi Poco X3 Pro", 
-        Precio:[285,265,200.99],
-        versiones:{
+        nombre:"Xiaomi Poco X3 Pro", 
+        precio:[285,265,200.99],
+        versiones:[{
+
             ram:"8GB", 
             memoria:["256GB", "128GB", "64GB"],
             procesador:"Snapdragon 860",
             bateria:"5260 mA",
             camara:["2Mp","8Mp","48Mp"]
-        }, 
+        }], 
          
         colores:["Negro","Rosa","Dorado","Blanco","Azul"], 
         origen:["China"],  
         fechasalida: new Date("2021-03-26"), 
-        android: true , 
-        gaming:false
+        android: true 
 }
 )
 
 db.productos.insertMany([
-    {_id:02,Nombre:"Huawei P30 Black", Precio:[294.99,210],versiones:{ram:"6GB", memoria:["256GB", "128GB", "64GB"],procesador:"Snapdragon 860",bateria:"5100 mA",camara:["2Mp","18Mp","64Mp"] }, colores:["Negro","Dorado","Blanco","Azul"], origen:["China"],fechasalida: new Date("2021-02-13"), android: true, gaming:false },
-    {_id:03,Nombre:"Huawei P Smart Z", Precio:[250.99, 180.99, 160.99],versiones:{ram:["8GB","6GB","4GB"], memoria:["128GB, 64GB, 32GB"],procesador:"Snapdragon 650",bateria:"3500 mA",camara:["8Mp","64Mp"] }, colores:["Negro","Dorado","Blanco","Amarillo","Turquesa"], origen:["China"],fechasalida: new Date("2019-09-18"), android: true, gaming:false },
-    {_id:04,Nombre:"Samsung Galaxy M12", Precio:229,versiones:{ram:"6GB", memoria:[ "128GB", "64GB"],procesador:"Exynos850",bateria:"6000 mA",camara:["2Mp","2Mp","18Mp","64Mp"] }, colores:["Negro","Blanco","Azul"], origen:["China"],fechasalida: new Date("2020-11-10"), android: true,gaming:false},
-    {_id:05,Nombre:"Iphone 13 Mini", Precio:[1229,1118,1000.99],versiones:{ram:["(8GB","6GB","12GB"], memoria:[ "226GB", "118GB","556GB"],procesador:"A15 Bionic",bateria:"6000 mA",camara:["18Mp","64Mp"] }, colores:["Negro","Blanco","Azul","Rojo","Verde","Amarillo","Morado"], origen:["USA"],fechasalida: new Date("2021-09-24"), android: false, gaming:false },
-    {_id:06,Nombre:"Huawei Honor 20", Precio:[230.99,185],versiones:{ram:["6GB","4GB"], memoria:[ "128GB", "64GB"],procesador:"Snapdragon 650",bateria:"3700 mA",camara:["8Mp","32Mp"] }, colores:["Negro","Blanco","Azul","Morado"], origen:["China"],fechasalida: new Date("2018-07-17"), android: true, gaming:false },
-    {_id:07,Nombre:"Motorola Moto E20", Precio:[109.99,120.99],versiones:{ram:["4GB","2GB"], memoria:[ "32GB", "64GB"],procesador:"UNISOC T606",bateria:"4000 mA",camara:["2Mp","32Mp"] }, colores:["Negro","Blanco","Azul","Morado","Amarillo","Rojo"], origen:["China"],fechasalida: new Date("2020-09-02"), android: true, gaming:false },
-    {_id:08,Nombre:"Samsung Galaxy A52s", Precio:[398.90],versiones:{ram:"6GB", memoria:"128GB",procesador:"Snapdragon 778G",bateria:"4500 mA",camara:["2Mp","5Mp","12Mp","64Mp"] }, colores:["Negro","Blanco","Azul","Morado","Amarillo","Rojo","Turquesa"], origen:["China"],fechasalida: new Date("2021-04-07"), android: true, gaming:false },
-    {_id:09,Nombre:"Iphone 12", Precio:[859],versiones:{ram:"8GB", memoria:"128GB",procesador:"A14 Bionic",bateria:"5000 mA",camara:["12Mp","64Mp"] }, colores:["Negro","Blanco","Azul","Morado","Amarillo","Rojo"], origen:["USA"],fechasalida: new Date("2020-10-13"), android: false, gaming:false },
-    {_id:10,Nombre:"Realme C11 2021", Precio:[109.99,80.99],versiones:{ram:["4GB","2GB"], memoria:["32GB","16GB"],procesador:"Helio G35",bateria:"5000 mA",camara:"12Mp" }, colores:["Negro","Blanco"], origen:["China"],fechasalida: new Date("2021-05-11"), android: true, gaming:false },
-    {_id:11,Nombre:"Black Shark 4", Precio:444.99,versiones:{ram:"8GB", memoria:"128GB",procesador:"Snapdragon 870 + SDX55, 3,2 GHz",bateria:"4500 mA",camara:["48Mp","12Mp","8Mp"] }, colores:["Negro","Blanco","Gris"], origen:["China"],fechasalida: new Date("2021-05-06"), android: true, gaming:true },
-    {_id:12,Nombre:"Xiaomi Pocophone F2 Pro", Precio:[432.99,499.99],versiones:{ram:["6GB","8GB"], memoria:["128GB","256GB"],procesador:"Snapdragon™ 865",bateria:"5000 mA",camara:"12Mp" }, colores:["Negro","Blanco","Azul","Morado","Marron"], origen:["China"],fechasalida: new Date("2021-07-22"), android: true, gaming:false }
+    {_id:02,nombre:"Huawei P30 Black", precio:[294.99,210],versiones:[{ram:"6GB", memoria:["256GB", "128GB", "64GB"],procesador:"Snapdragon 860",bateria:"5100 mA",camara:["2Mp","18Mp","64Mp"] }], colores:["Negro","Dorado","Blanco","Azul"], origen:["China"],fechasalida: new Date("2021-02-13"), android: true },
+    {_id:03,nombre:"Huawei P Smart Z", precio:[250.99, 180.99, 160.99],versiones:[{ram:["8GB","6GB","4GB"], memoria:["128GB, 64GB, 32GB"],procesador:"Snapdragon 650",bateria:"3500 mA",camara:["8Mp","64Mp"] }], colores:["Negro","Dorado","Blanco","Amarillo","Turquesa"], origen:["China"],fechasalida: new Date("2019-09-18"), android: true},
+    {_id:04,nombre:"Samsung Galaxy M12", precio:229,versiones:[{ram:"6GB", memoria:[ "128GB", "64GB"],procesador:"Exynos850",bateria:"6000 mA",camara:["2Mp","2Mp","18Mp","64Mp"] }], colores:["Negro","Blanco","Azul"], origen:["China"],fechasalida: new Date("2020-11-10"), android: true},
+    {_id:05,nombre:"Iphone 13 Mini", precio:[1229,1118,1000.99],versiones:[{ram:["(8GB","6GB","12GB"], memoria:[ "226GB", "118GB","556GB"],procesador:"A15 Bionic",bateria:"6000 mA",camara:["18Mp","64Mp"] }], colores:["Negro","Blanco","Azul","Rojo","Verde","Amarillo","Morado"], origen:["USA"],fechasalida: new Date("2021-09-24"), android: false},
+    {_id:06,nombre:"Huawei Honor 20", precio:[230.99,185],versiones:[{ram:["6GB","4GB"], memoria:[ "128GB", "64GB"],procesador:"Snapdragon 650",bateria:"3700 mA",camara:["8Mp","32Mp"] }], colores:["Negro","Blanco","Azul","Morado"], origen:["China"],fechasalida: new Date("2018-07-17")},
+    {_id:07,nombre:"Motorola Moto E20", precio:[109.99,120.99],versiones:[{ram:["4GB","2GB"], memoria:[ "32GB", "64GB"],procesador:"UNISOC T606",bateria:"4000 mA",camara:["2Mp","32Mp"] }], colores:["Negro","Blanco","Azul","Morado","Amarillo","Rojo"], origen:["China"],fechasalida: new Date("2020-09-02"), android: true},
+    {_id:08,nombre:"Samsung Galaxy A52s", precio:[398.90],versiones:[{ram:"6GB", memoria:"128GB",procesador:"Snapdragon 778G",bateria:"4500 mA",camara:["2Mp","5Mp","12Mp","64Mp"] }], colores:["Negro","Blanco","Azul","Morado","Amarillo","Rojo","Turquesa"], origen:["China"],fechasalida: new Date("2021-04-07"), android: true},
+    {_id:09,nombre:"Iphone 12", precio:[859],versiones:[{ram:"8GB", memoria:"128GB",procesador:"A14 Bionic",bateria:"5000 mA",camara:["12Mp","64Mp"] }], colores:["Negro","Blanco","Azul","Morado","Amarillo","Rojo"], origen:["USA"],fechasalida: new Date("2020-10-13"), android: false},
+    {_id:10,nombre:"Realme C11 2021", precio:[109.99,80.99],versiones:[{ram:["4GB","2GB"], memoria:["32GB","16GB"],procesador:"Helio G35",bateria:"5000 mA",camara:"12Mp" }], colores:["Negro","Blanco"], origen:["China"],fechasalida: new Date("2021-05-11"), android: true},
+    {_id:11,nombre:"Black Shark 4", precio:444.99,versiones:[{ram:"8GB", memoria:"128GB",procesador:"Snapdragon 870 + SDX55, 3,2 GHz",bateria:"4500 mA",camara:["48Mp","12Mp","8Mp"] }], colores:["Negro","Blanco","Gris"], origen:["China"],fechasalida: new Date("2021-05-06"), android: true, gaming:true },
+    {_id:12,nombre:"Xiaomi Pocophone F2 Pro", precio:[432.99,499.99],versiones:[{ram:["6GB","8GB"], memoria:["128GB","256GB"],procesador:"Snapdragon™ 865",bateria:"5000 mA",camara:"12Mp" }], colores:["Negro","Blanco","Azul","Morado","Marron"], origen:["China"],fechasalida: new Date("2021-07-22"), android: true}
 
 ])
 
